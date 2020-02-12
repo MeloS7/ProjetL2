c812c6f0d9c5d4b7f8a197d9bbfcbe43f39ca459
MeloS7 <kltd1999@163.com>
2020-02-12 00:16:56 -0800

    12/2

---
+++ Awele/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import awele
+import sys
+sys.path.append("..")
+import game
+game.game=awele
+sys.path.append("./Joueurs")
+import joueur_humain
+game.joueur1=joueur_humain
+game.joueur2=joueur_humain
+
+
